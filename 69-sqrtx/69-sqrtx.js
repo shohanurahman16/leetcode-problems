be69ae0952bd6f81f062241cb045a0e49f70eea5
Md. Mehedi Hasan <mehedi.iitdu@gmail.com>
2022-08-25 10:47:06 +0600

    Time: 126 ms (42.93%), Space: 44.1 MB (28.87%) - LeetHub

---
+++ 69-sqrtx/69-sqrtx.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function(x) {
+    for (let i = 0; i <= x/2 + 1; i++){
+        if(i*i == x){
+            return i
+        }
+        else if(i*i > x){
+            return i - 1
+        }
+    }
+};
