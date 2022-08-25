b6b325f06d6c5bd356494e45821f1058f060c3d4
Md. Mehedi Hasan <mehedi.iitdu@gmail.com>
2022-08-25 10:48:47 +0600

    Time: 105 ms (20.99%), Space: 42.7 MB (10.94%) - LeetHub

---
+++ 35-search-insert-position/35-search-insert-position.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    let len = nums.length;
+     for(let i=0; i< len; i++) {
+        if(target == nums[i] || target < nums[i]) {
+            return i
+        }
+    }
+    
+    return len
+};
