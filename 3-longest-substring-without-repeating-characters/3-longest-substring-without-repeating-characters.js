b98e60a68833c34bf5ac3f448380ce5124694ca8
Md. Mehedi Hasan <mehedi.iitdu@gmail.com>
2022-09-01 22:22:41 +0600

    Time: 1113 ms (5.34%), Space: 49.4 MB (20.58%) - LeetHub

---
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    let maxLength = 0;
+    let localMax = 0;
+    let subStr = []
+    let i = 0
+    
+    while(i < s.length){
+        if(subStr.indexOf(s[i]) >= 0){
+            s = s.substring(subStr.indexOf(s[i]) + 1)
+            i = 0
+            subStr = []
+            localMax = 0
+        }
+        else{
+            subStr.push(s[i])
+            i++
+            localMax++
+        }
+        
+        if(localMax > maxLength){
+            maxLength = localMax
+        }
+    }
+    
+    return maxLength;
+};
