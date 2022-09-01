6bfac280830d025f0edb41ec9f377e409bc3fd8d
Md. Mehedi Hasan <mehedi.iitdu@gmail.com>
2022-09-01 12:32:11 +0600

    Time: 193 ms (23.85%), Space: 56.5 MB (13.20%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.js
+++ 238-product-of-array-except-self/238-product-of-array-except-self.js
@@ -3,20 +3,20 @@
  * @return {number[]}
  */
 var productExceptSelf = function(nums) {
-    let output = []
+    let output = [1]
     let prefix = 1;
     let postfix = 1;
     
     // calculate prefix
-    for(let i = 0; i < nums.length; i++){
-        let currentPrefix = prefix * (nums[i-1] != undefined ? nums[i-1] : 1)
+    for(let i = 1; i < nums.length; i++){
+        let currentPrefix = prefix * nums[i-1]
         output.push(currentPrefix)
         prefix = currentPrefix
     }
     
     // calculate postfix and output array
-    for(let i = nums.length - 1; i >= 0; i--){
-        let currentPostfix = postfix * (nums[i+1] != undefined ? nums[i+1] : 1)
+    for(let i = nums.length - 2; i >= 0; i--){
+        let currentPostfix = postfix * nums[i+1]
         output[i] = output[i] * currentPostfix
         postfix = currentPostfix
     }
