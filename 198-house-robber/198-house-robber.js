e9626ea77b115ab3ede8e5e7f3dfa774ff422b0a
Md. Mehedi Hasan <mehedi.iitdu@gmail.com>
2022-09-05 02:14:51 +0600

    Time: 106 ms (17.25%), Space: 42.4 MB (8.20%) - LeetHub

--- 198-house-robber/198-house-robber.js
+++ 198-house-robber/198-house-robber.js
@@ -3,11 +3,15 @@
  * @return {number}
  */
 var rob = function(nums) {
-    let dp = []
-    dp[-1] = 0
-    dp[0]  = nums[0]
+    if(nums.length < 2){
+        return nums[0]
+    }
     
-    for(let i=1; i<nums.length; i++){
+    let dp = []
+    dp[0]  = nums[0]
+    dp[1]  = Math.max(nums[0], nums[1])
+    
+    for(let i=2; i < nums.length; i++){
         dp[i] = Math.max(dp[i-1], dp[i-2] + nums[i])
     }
     
