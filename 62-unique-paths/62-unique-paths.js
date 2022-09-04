09b82be87c576b578ebed529227f0622d16ab8aa
Md. Mehedi Hasan <mehedi.iitdu@gmail.com>
2022-09-04 21:30:50 +0600

    Time: 69 ms (87.00%), Space: 42.3 MB (53.25%) - LeetHub

--- 62-unique-paths/62-unique-paths.js
+++ 62-unique-paths/62-unique-paths.js
@@ -4,11 +4,33 @@
  * @return {number}
  */
 var uniquePaths = function(m, n, memo = {}) {
-    const key = `${m},${n}`;
-    if (key in memo) return memo[key]
-    if (m == 1 && n == 1) return 1
-    if (m == 0 || n ==0) return 0
+    // recursion
+//     const key = `${m},${n}`;
+//     if (key in memo) return memo[key]
+//     if (m == 1 && n == 1) return 1
+//     if (m == 0 || n ==0) return 0
     
-    memo[key] =  uniquePaths(m-1, n, memo) + uniquePaths(m, n-1, memo)
-    return memo[key]
+//     memo[key] =  uniquePaths(m-1, n, memo) + uniquePaths(m, n-1, memo)
+//     return memo[key]
+    
+    let dp = []
+    for(let i=0; i <= m; i++){
+        dp[i] = []
+    }
+    
+    for(let i=0; i <= m; i++){
+        for(let j=0; j <= n; j++){
+            if(i== 0 || j == 0){
+                dp[i][j] = 0
+            }
+            else if(i==1 && j==1){
+                dp[i][j] = 1
+            }
+            else{
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+    }
+    
+    return dp[m][n]
 };
