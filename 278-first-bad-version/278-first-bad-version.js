b37d1b3d88cd9ed41e38e79e60b778ce3599dbe6
Md. Mehedi Hasan <mehedi.iitdu@gmail.com>
2022-09-05 23:33:44 +0600

    Time: 4656 ms (5.01%), Space: 41.6 MB (77.85%) - LeetHub

---
+++ 278-first-bad-version/278-first-bad-version.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for isBadVersion()
+ * 
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function(isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function(n) {
+        while(n >= 1){
+            if (!isBadVersion(n)) return n+1
+            n--
+        }
+        
+        return n == 0 ? 1 : n
+    };
+};
