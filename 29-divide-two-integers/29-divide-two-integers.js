429b1f6ad3105e50dd6b39c7967b1c38ff6240b4
Md. Mehedi Hasan <mehedi.iitdu@gmail.com>
2022-09-05 14:17:38 +0600

    Time: 4505 ms (19.92%), Space: 44.1 MB (54.62%) - LeetHub

---
+++ 29-divide-two-integers/29-divide-two-integers.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} dividend
+ * @param {number} divisor
+ * @return {number}
+ */
+var divide = function(dividend, divisor) {
+    if (dividend === -Math.pow(2, 31) && divisor === -1) return Math.pow(2, 31) - 1;
+    if (divisor === 1) return dividend;
+    if (dividend === divisor) return 1;
+    
+    let result = 0
+    let absulateDividend = Math.abs(dividend)
+    let absulateDivisor  = Math.abs(divisor)
+    
+    while(absulateDividend >= absulateDivisor){
+        absulateDividend = absulateDividend - absulateDivisor
+        result++
+    }
+    
+    return (dividend>0 && divisor<0) || (dividend<0 && divisor>0) ? (-1)*result : result
+};
