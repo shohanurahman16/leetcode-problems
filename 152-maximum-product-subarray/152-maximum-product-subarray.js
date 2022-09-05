c2a766d8e5f0e6416ae84936f6ddb6fc845a815c
Md. Mehedi Hasan <mehedi.iitdu@gmail.com>
2022-09-05 18:08:35 +0600

    Time: 153 ms (5.00%), Space: 47.1 MB (5.09%) - LeetHub

---
+++ 152-maximum-product-subarray/152-maximum-product-subarray.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxProduct = function(nums) {
+    let dpMax = []
+    let dpMin = []
+    let result = nums[0]
+    dpMax[0] = nums[0]
+    dpMin[0] = nums[0]
+    
+    for(let i = 1; i < nums.length; i++){
+        dpMax[i] = Math.max(dpMax[i-1] * nums[i], dpMin[i-1] * nums[i], nums[i])
+        dpMin[i] = Math.min(dpMin[i-1] * nums[i], dpMax[i-1] * nums[i], nums[i])
+        
+        result = Math.max(result, dpMax[i])
+    }
+    
+    console.log(dpMax, dpMin)
+    
+    return result
+};
