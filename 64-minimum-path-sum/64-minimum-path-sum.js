71f21b0e804e0eeac45dfcaf63e2379231f2e33d
Md. Mehedi Hasan <mehedi.iitdu@gmail.com>
2022-09-07 13:15:41 +0600

    Time: 119 ms (33.15%), Space: 44.4 MB (60.30%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.js
+++ 64-minimum-path-sum/64-minimum-path-sum.js
@@ -6,18 +6,46 @@ var minPathSum = function(grid) {
     let m = grid.length
     let n = grid[0].length
     
-    function DFS(i, j, memo = {}){
-        const objectKey = `${i}-${j}`
+    
+    // DFS solution
+//     function DFS(i, j, memo = {}){
+//         const objectKey = `${i}-${j}`
         
-        if(memo[objectKey]) return memo[objectKey]
+//         if(memo[objectKey]) return memo[objectKey]
         
-        if(i < 0 || j < 0) return Number.MAX_SAFE_INTEGER
-        if(i==0 && j==0) return grid[i][j]
+//         if(i < 0 || j < 0) return Number.MAX_SAFE_INTEGER
+//         if(i==0 && j==0) return grid[i][j]
         
-        memo[objectKey] = Math.min(DFS(i-1, j, memo), DFS(i, j-1, memo)) + grid[i][j]
+//         memo[objectKey] = Math.min(DFS(i-1, j, memo), DFS(i, j-1, memo)) + grid[i][j]
         
-        return memo[objectKey]
+//         return memo[objectKey]
+//     }
+    
+//     return DFS(m-1, n-1)
+    
+    // BFS solution
+    
+    let dp = []
+    for(let i=0; i <= m; i++){
+        dp[i] = []
     }
     
-    return DFS(m-1, n-1)
+    for(let i=0; i< m; i++){
+        for(let j=0; j <n; j++){
+            if(i> 0 && j > 0){
+                dp[i][j] = Math.min(dp[i-1][j], dp[i][j-1]) + grid[i][j]
+            }
+            else if(i > 0){
+                dp[i][j] = dp[i-1][j] + grid[i][j]
+            }
+            else if(j > 0){
+                 dp[i][j] = dp[i][j-1] + grid[i][j]
+            }
+            else{
+                dp[i][j] = grid[i][j]
+            }
+        }
+    }
+    
+    return dp[m-1][n-1]
 };
